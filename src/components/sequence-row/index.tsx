4f50a7ed7708b5bdfe2a1a1c9cb6054c687f6961
d.mishin <d.mishin@rambler-co.ru>
2023-09-24 22:13:33 +0300

    weap: working on dnd

--- src/components/sequence-row/index.tsx
+++ src/components/sequence-row/index.tsx
@@ -1,14 +1,17 @@
 import React, {useCallback, useMemo} from 'react'
 import type {Asana} from 'types'
 
-import styles from './styles.module.css'
 import {imageSrc} from 'lib/image-src'
 import {Button} from 'antd'
 import {CloseOutlined} from '@ant-design/icons'
 import {Input} from 'components/input'
+import {Droppable, Draggable} from 'react-beautiful-dnd'
+import styles from './styles.module.css'
+import clsx from 'clsx'
 
 interface SequenceRowProps {
   data: Asana[]
+  id: string
   title?: string
   onDelete: (id: number) => void
   onChange: (title: string) => void
@@ -17,23 +20,40 @@ interface SequenceRowProps {
 export const SequenceRow: React.FC<SequenceRowProps> = ({
   data = [],
   onDelete,
-  onChange
+  onChange,
+  id
 }) => {
   const sequence = useMemo(() => {
-    return data.map(({pk, image}, index) => (
-      <div className={styles.imageWrapper} key={index}>
-        <img width={70} height={70} key={pk} src={imageSrc(image)} />
-        <Button
-          shape="circle"
-          type="primary"
-          size="small"
-          danger
-          icon={<CloseOutlined />}
-          className={styles.deleteButton}
-          onClick={() => onDelete(index)}
-        />
-      </div>
-    ))
+    return data.map(({pk, image}, index) => {
+      return (
+        <Draggable
+          index={index}
+          draggableId={`asana-${pk}`}
+          key={`asana-${pk}`}>
+          {({draggableProps, dragHandleProps, innerRef}, {isDragging}) => (
+            <div
+              className={clsx(
+                styles.imageWrapper,
+                isDragging && styles.dragging
+              )}
+              ref={innerRef}
+              {...draggableProps}
+              {...dragHandleProps}>
+              <img width={70} height={70} key={pk} src={imageSrc(image)} />
+              <Button
+                shape="circle"
+                type="primary"
+                size="small"
+                danger
+                icon={<CloseOutlined />}
+                className={styles.deleteButton}
+                onClick={() => onDelete(index)}
+              />
+            </div>
+          )}
+        </Draggable>
+      )
+    })
   }, [data, onDelete])
 
   const onInputChange = useCallback(
@@ -45,6 +65,22 @@ export const SequenceRow: React.FC<SequenceRowProps> = ({
     [onChange]
   )
 
+  const droppable = useMemo(() => {
+    return (
+      <Droppable droppableId={id} direction="horizontal">
+        {({innerRef, placeholder, droppableProps}, {isDraggingOver}) => (
+          <div
+            ref={innerRef}
+            className={clsx(styles.sequence, isDraggingOver && styles.active)}
+            {...droppableProps}>
+            {sequence}
+            {placeholder}
+          </div>
+        )}
+      </Droppable>
+    )
+  }, [id, sequence])
+
   return (
     <div className={styles.sequenceRow}>
       <Input
@@ -54,7 +90,15 @@ export const SequenceRow: React.FC<SequenceRowProps> = ({
         label="Заголовок"
         name="title"
       />
-      <div className={styles.sequence}>{sequence}</div>
+      {droppable}
+      {/* <Droppable droppableId={id}>
+        {({innerRef, placeholder, droppableProps}) => (
+          <div ref={innerRef} className={styles.sequence} {...droppableProps}>
+            {sequence}
+            {placeholder}
+          </div>
+        )}
+      </Droppable> */}
     </div>
   )
 }
--- src/pages/sequence/create/index.page.tsx
+++ src/pages/sequence/create/index.page.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import React, {useCallback, useMemo, useState} from 'react'
 
 import styles from './styles.module.css'
 import {Spinner} from 'components/spinner'
@@ -12,6 +12,8 @@ import {PDFDocument} from 'components/pdf-viewer/document'
 import {saveAs} from 'file-saver'
 import {pdf} from '@react-pdf/renderer'
 import {SequenceRow} from 'components/sequence-row'
+import {DragDropContext, resetServerContext} from 'react-beautiful-dnd'
+import type {GetServerSideProps} from 'next/types'
 
 interface Sequence {
   title?: string
@@ -23,16 +25,60 @@ const initialSequence: Sequence = {
   asanas: []
 }
 
+interface BuilderData {
+  asanas: {
+    [key: string]: Asana
+  }
+  sequences: {
+    [key: string]: {
+      id: string
+      title: string
+      asanaIds: string[]
+    }
+  }
+  sequenceOrder: string[]
+}
+
+const initialBuilderData: BuilderData = {
+  asanas: {},
+  sequences: {},
+  sequenceOrder: ['sequence-1']
+}
+
 const CreateSequencePage: React.FC = () => {
+  const [builderData, setBuilderData] =
+    useState<BuilderData>(initialBuilderData)
+
   const [sequence, setSequence] = useState<Sequence>(initialSequence)
   const [isModelVisible, setIsModelVisible] = useState(false)
   const {isFetching, asanas} = useAsana()
 
-  const onAsanaClick = useCallback((asana: Asana) => {
-    setSequence(({title, asanas}) => ({
-      title,
-      asanas: [...asanas, asana]
-    }))
+  const onAsanaClick = useCallback(({pk, ...restData}: Asana) => {
+    setBuilderData((prevData) => {
+      const prevBuilderSequences = prevData.sequences['sequence-1']
+
+      const newBuilderData: BuilderData = {
+        asanas: {
+          ...prevData.asanas,
+          [`asana-${pk}`]: {
+            pk,
+            ...restData
+          }
+        },
+        sequences: {
+          ...prevData.sequences,
+          // TODO
+          'sequence-1': {
+            ...prevBuilderSequences,
+            id: 'sequence-1',
+            asanaIds: [...(prevBuilderSequences?.asanaIds ?? []), `asana-${pk}`]
+          }
+        },
+        sequenceOrder: [...prevData.sequenceOrder]
+      }
+
+      return newBuilderData
+    })
   }, [])
 
   const deleteAsanaById = useCallback((id: number): void => {
@@ -61,6 +107,87 @@ const CreateSequencePage: React.FC = () => {
     setSequence(({asanas}) => ({asanas, title}))
   }, [])
 
+  const onDragStart = useCallback((data) => {
+    console.log('start', data)
+  }, [])
+
+  const onDragUpdate = useCallback((data) => {
+    console.log('update', data)
+  }, [])
+
+  const onDragEnd = useCallback(
+    ({source, destination, draggableId}) => {
+      if (!destination) {
+        return
+      }
+
+      if (
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index
+      ) {
+        return
+      }
+
+      const sequence = builderData.sequences[source.droppableId]
+      const newAsanasIds = [...sequence.asanaIds]
+
+      newAsanasIds.splice(source.index, 1)
+      newAsanasIds.splice(destination.index, 0, draggableId)
+
+      const newSequence = {
+        ...sequence,
+        asanaIds: newAsanasIds
+      }
+
+      const newBuilderData = {
+        ...builderData,
+        sequences: {
+          ...builderData.sequences,
+          [newSequence.id]: newSequence
+        }
+      }
+
+      console.log(newBuilderData, builderData)
+
+      setBuilderData(newBuilderData)
+    },
+    [builderData]
+  )
+
+  const sequences = useMemo(() => {
+    return builderData.sequenceOrder.map((sequenceId) => {
+      const currentSequence = builderData.sequences[sequenceId]
+
+      const data = (currentSequence?.asanaIds ?? []).map(
+        (id) => builderData.asanas[id]
+      )
+
+      return (
+        <DragDropContext
+          onDragEnd={onDragEnd}
+          onDragStart={onDragStart}
+          onDragUpdate={onDragUpdate}
+          key={sequenceId}>
+          <SequenceRow
+            data={data}
+            onDelete={deleteAsanaById}
+            onChange={onSequenceTitleChange}
+            id={sequenceId}
+          />
+        </DragDropContext>
+      )
+    })
+  }, [
+    builderData.sequenceOrder,
+    builderData.sequences,
+    builderData.asanas,
+    onDragEnd,
+    onDragStart,
+    onDragUpdate,
+    deleteAsanaById,
+    onSequenceTitleChange
+  ])
+
   if (isFetching) {
     return <Spinner />
   }
@@ -76,13 +203,8 @@ const CreateSequencePage: React.FC = () => {
         />
       </div>
       <div className={styles.previewWrapper}>
-        <div className={styles.sequences}>
-          <SequenceRow
-            data={sequence.asanas}
-            onDelete={deleteAsanaById}
-            onChange={onSequenceTitleChange}
-          />
-        </div>
+        <div className={styles.sequences}>{sequences}</div>
+
         <div className={styles.actionButtons}>
           <Button
             type="primary"
@@ -124,3 +246,10 @@ const CreateSequencePage: React.FC = () => {
 }
 
 export default CreateSequencePage
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // reset dnd context to prevent didn't match error
+  resetServerContext()
+
+  return {props: {data: []}}
+}
