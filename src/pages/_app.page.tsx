0ec7300ded8102205a05cce432e89c60434ebeb1
Mishin Dmitry <dmishin.dev@gmail.com>
2023-10-16 17:48:28 +0300

    returned is page loading

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,13 +5,13 @@ import type {AppProps} from 'next/app'
 import {ErrorBoundary} from 'components/error-boundary'
 import {Layout} from 'components/layout'
 import {activateYandexMetrika} from 'lib/metrics'
-// import {usePageLoading} from 'lib/use-page-loading'
+import {usePageLoading} from 'lib/use-page-loading'
 
-// import {Spinner} from 'components/spinner'
+import {Spinner} from 'components/spinner'
 import 'styles/global.css'
 
 const App: React.FC<AppProps> = ({Component, pageProps = {}}) => {
-  // const {isPageLoading} = usePageLoading()
+  const {isPageLoading} = usePageLoading()
 
   useEffect(() => {
     activateYandexMetrika()
@@ -21,8 +21,7 @@ const App: React.FC<AppProps> = ({Component, pageProps = {}}) => {
     <>
       <ErrorBoundary fallback={<h1>Что то пошло не так...</h1>}>
         <Layout isMobile={pageProps.isMobile}>
-          <Component {...pageProps} />
-          {/* {isPageLoading ? <Spinner /> : <Component {...pageProps} />} */}
+          {isPageLoading ? <Spinner /> : <Component {...pageProps} />}
         </Layout>
       </ErrorBoundary>
     </>
